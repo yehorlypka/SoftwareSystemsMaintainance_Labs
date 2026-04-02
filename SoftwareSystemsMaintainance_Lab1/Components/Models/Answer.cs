c2a4e6eb7ee95eeb2b71c6b78235eea49560cb9c
Yehor Lypka <yehor.lypka@student.uzhnu.edu.ua>
2026-04-02 16:22:21 +0300

    Task 2: fixed JSON parsing bugs

--- SoftwareSystemsMaintainance_Lab1/Components/Models/Answer.cs
+++ SoftwareSystemsMaintainance_Lab1/Components/Models/Answer.cs
@@ -1,10 +1,18 @@
 namespace SoftwareSystemsMaintainance_Lab1.Components.Models;
 
+using System.Text.Json.Serialization;
+
 public class Answer
 {
+    [JsonRequired]
     public int Id { get; set; }
+    
+    [JsonRequired]
     public int Correct { get; set; }
+    
+    [JsonRequired]
     public int? Value { get; set; }
+    
     public double Score
     {
         get
@@ -21,7 +29,7 @@ public class Answer
             return 1.0 - (t * t * (3.0 - 2.0 * t));
         }
     }
-
+    
     public double Error
     {
         get
--- SoftwareSystemsMaintainance_Lab1/Components/Models/Question.cs
+++ SoftwareSystemsMaintainance_Lab1/Components/Models/Question.cs
@@ -5,11 +5,14 @@ using System.Text.Json.Serialization;
 public class Question
 {
     [JsonPropertyName("id")]
+    [JsonRequired]
     public int Id { get; set; }
         
     [JsonPropertyName("question")]
+    [JsonRequired]
     public string QuestionText { get; set; }
         
     [JsonPropertyName("correct")]
+    [JsonRequired]
     public int Correct { get; set; }
 }
