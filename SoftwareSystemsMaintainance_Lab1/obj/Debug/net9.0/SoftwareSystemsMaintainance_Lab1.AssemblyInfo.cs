256d61d6ec77b48a286537326c913e950fa4981d
Yehor Lypka <yehor.lypka@student.uzhnu.edu.ua>
2026-05-15 07:57:23 +0300

    Task 4. Added new functionality - question set creation

--- SoftwareSystemsMaintainance_Lab1/obj/Debug/net9.0/SoftwareSystemsMaintainance_Lab1.AssemblyInfo.cs
+++ SoftwareSystemsMaintainance_Lab1/obj/Debug/net9.0/SoftwareSystemsMaintainance_Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftwareSystemsMaintainance_Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a3c8013be322572d189434cd581bcfceaf7d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a4e6eb7ee95eeb2b71c6b78235eea49560cb9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftwareSystemsMaintainance_Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftwareSystemsMaintainance_Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
